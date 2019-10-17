38ee042b2ca87a4d33ac20d4f2bec9b7813e3830
Camille FOSSIER <camille.fossier@gmail.com>
2019-10-17 18:42:46 +0200

    better indentation and some comments

--- src/simulacionesMISE_par.R
+++ src/simulacionesMISE_par.R
@@ -1,46 +1,45 @@
 library(parallel)
 library(ks)
 
-
 source("functions2.r")
 
 simulaciones=function(n=100,M=10,B=150){
- 
+  # Kernel smoothing
   require(ks)
+  # Implementation of distributions for nonparametric density estimation
   require(benchden)
   AA=matrix(0,nrow=6,ncol=7)
   colnames(AA)=c("H","FP","Kde", "BagHist", "BagHistFP", "Bagkde", "Rash")
   rownames(AA)=c("normal","chi2","mezcla1","mezcla2","bart","triangular")
   for(i in 1:M){
-    
-   
-   A=matrix(NA,nrow=6,ncol=7)
-   j=1
-  for(ll in c(1,3,5,7,8,11)){
-    
-    dd=gendata(ll,n)
-    bopt=bropt(dd$train)$opt
-    zz=hist(dd$train,breaks=mybreaks(dd$train,nbr=bopt),plot=F)
-    
-    #bhist=BagHistfp(xx=dd$train,grille=dd$test,nbr = bopt, B)
-    bhist=BagHistfp(xx=dd$train,grille=dd$test, B)
-    modelrash=rash(dd$train,grille=dd$test,nbr = bopt, B)
-    #modelavshift(dd$train,dd$test,nbr=bopt,M=B)
-    modelbagkde <- Bagkde(xx = dd$train, grille = dd$test, B)
-    
-  A[j,]=c(error(dd$dobs,predict.hist(zz,dd$test))[1],
-           error(dd$dobs,approxfun(x=zz$mids,y=zz$density)(dd$test))[1],    
-           error(dd$dobs,onekdeucv(dd$train,dd$test))[1],
-           error(dd$dobs,bhist$bh)[1],
-           error(dd$dobs,bhist$bhfp)[1], 
-           #error(dd$dobs,modelavshift)[1]
-           error(dd$dobs,modelbagkde)[1],
-           error(dd$dobs,modelrash)[1] )
-  
-  #MISE[j,]=(dd$dobs-bhist$bhfp)^2 +
-  j=j+1
+      
+    A=matrix(NA,nrow=6,ncol=7)
+    j=1
+    for(ll in c(1,3,5,7,8,11)){
+      
+      dd=gendata(ll,n)
+      bopt=bropt(dd$train)$opt
+      zz=hist(dd$train,breaks=mybreaks(dd$train,nbr=bopt),plot=F)
+      
+      #bhist=BagHistfp(xx=dd$train,grille=dd$test,nbr = bopt, B)
+      bhist=BagHistfp(xx=dd$train,grille=dd$test, B)
+      modelrash=rash(dd$train,grille=dd$test,nbr = bopt, B)
+      #modelavshift(dd$train,dd$test,nbr=bopt,M=B)
+      modelbagkde <- Bagkde(xx = dd$train, grille = dd$test, B)
+      
+      A[j,]=c(error(dd$dobs,predict.hist(zz,dd$test))[1],
+               error(dd$dobs,approxfun(x=zz$mids,y=zz$density)(dd$test))[1],    
+               error(dd$dobs,onekdeucv(dd$train,dd$test))[1],
+               error(dd$dobs,bhist$bh)[1],
+               error(dd$dobs,bhist$bhfp)[1], 
+               #error(dd$dobs,modelavshift)[1]
+               error(dd$dobs,modelbagkde)[1],
+               error(dd$dobs,modelrash)[1] )
+      
+      #MISE[j,]=(dd$dobs-bhist$bhfp)^2 +
+      j=j+1
     }
-  AA=AA+A
+    AA=AA+A
   }
   AA=AA/M
 }
